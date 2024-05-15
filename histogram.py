8b5abb54699ca63d45a455c29b1834ba4bba416d
kondasweth <swetha1.konda@jukshio.com>
2024-05-15 11:07:39 +0530

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("image_path", help = "Enter the path of the image")
+
+parser.add_argument("out_dir", help = "name of the output directory where you want to save your output")
+
+args = parser.parse_args()
+image_dir = args.image_path
  
 img = cv.imread('/home/swetha-konda/Downloads/parrot.jpeg')
 cv.imwrite("/home/swetha-konda/Downloads/prt.jpeg",img)
